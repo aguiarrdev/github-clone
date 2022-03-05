442c8ba43268d808d6b59ea0bf4bc1895ed53167
Matheus Aguiar <matheus.aguiar@apiki.com>
2022-03-05 03:12:16 -0300

    feat: iniciando desenvolvimento da página de profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { Container, Main, LeftSide, RightSide } from './styles';
+import ProfileData from '../../components/ProfileData';
+
 const Profile: React.FC = () => {
-    return <div/>;
+    return (
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData
+                        username={'aguiarrdev'}
+                        name={'Matheus Aguiar'}
+                        avatarUrl={'https://avatars.githubusercontent.com/u/71287681?v=4'}
+                        followers={10}
+                        following={7}
+                        company={'Company'}
+                        location={'Gov. Valadares, Brasil'}
+                        email={'aguiartgv@gmail.com'}
+                        blog={'linkedin.com/in/aguiarrdev'}
+                    >
+
+                    </ProfileData>
+                </LeftSide>
+                <RightSide></RightSide>
+            </Main>
+        </Container>
+    )
 };
 
 export default Profile;
