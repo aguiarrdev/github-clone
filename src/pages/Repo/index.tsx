532e274b325dd9df398a8b0df4fe1990d459025e
Matheus Aguiar <matheus.aguiar@apiki.com>
2022-03-17 20:16:04 -0300

    feat: criando página de repositórios

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  Container,
+  BreadCrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-    return <div/>;
+  return (
+    <Container>
+      <BreadCrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/aguiarrdev"}>
+          aguiarrdev
+        </Link>
+        <span>/</span>
+        <Link className={"reponame"} to={"/aguiarrdev/github-clone"}>
+          github-clone
+        </Link>
+      </BreadCrumb>
+
+      <p>This is a Github clone made with ReactJS</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>5</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>1</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/aguiarrdev/github-clone"}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
